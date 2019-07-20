98f59d5c4217e1c8cb3aae5a501094a976920468
Heiko Noordhof <h.e.noordhof@rug.nl>
2019-07-20 19:23:29 +0200

    knoppen en checkboxes bij barcodes toevoegen

--- barcodeswidget.py
+++ barcodeswidget.py
@@ -21,22 +21,44 @@ class BarcodesWidget(Gtk.Grid):
         self.lijst = DataLijst(columns, show_primary_key=True, expand=True, multiselect=True)
         self.lijst.view.connect("row-activated", self.on_row_activated)
 
-        nieuw_button = Gtk.Button("Nieuwe streepjescodes")
+        nieuw_button = Gtk.Button("Genereer nieuwe barcodes")
         nieuw_button.connect("clicked", self.on_nieuw)
+        verwijder_button = Gtk.Button("Verwijder")
+        verwijder_button.connect("clicked", self.on_verwijder)
         print_button = Gtk.Button("Print")
         print_button.connect("clicked", self.on_print)
 
+        bestaand_check = Gtk.CheckButton(label="Inclusief bestaande boeken")
+        geprint_check = Gtk.CheckButton(label="Reeds geprinte barcodes")
+
         # layout
         #
+        knoppen = Gtk.Grid(
+                margin=12,
+                column_spacing=8,
+                row_spacing=8,
+            )
+        knoppen.attach(nieuw_button, 1, 0, 1, 1)
+        knoppen.attach(verwijder_button, 1, 1, 1, 1)
+        knoppen.attach(print_button, 1, 2, 1, 1)
+
+        selectie = Gtk.Grid(
+                margin=12,
+                column_spacing=8,
+                row_spacing=8,
+            )
+        selectie.attach(bestaand_check, 1, 0, 1, 1)
+        selectie.attach(geprint_check, 1, 1, 1, 1)
+
         paneel = Gtk.Grid(
                 margin=8,
                 column_spacing=8,
                 row_spacing=8,
                 halign=Gtk.Align.END,
             )
-        paneel.attach(Gtk.Label(), 0, 0, 1, 1)
-        paneel.attach(nieuw_button, 1, 0, 1, 1)
-        paneel.attach(print_button, 1, 1, 1, 1)
+        #paneel.attach(Gtk.Label(), 0, 0, 1, 1)
+        paneel.attach(selectie, 0, 0, 1, 1)
+        paneel.attach(knoppen, 1, 0, 1, 1)
 
         self.add(paneel)
         self.add(self.lijst)
@@ -45,6 +67,9 @@ class BarcodesWidget(Gtk.Grid):
     def refresh(self):
         self.barcodes = self.db.get_barcodes(with_book=None)
         self.lijst.load(self.barcodes)
+
+    def on_verwijder(self, button):
+        print("verwijderen barcode niet geimplemtneerd nog")
 
     def on_nieuw(self, button):
         dialog = CategorieDialog(self.parent, self.db)
@@ -74,7 +99,6 @@ class BarcodesWidget(Gtk.Grid):
         selection = self.lijst.view.get_selection()
         endpath = Gtk.TreePath(end_idx)
         selection.select_range(path, endpath)
-        
 
     def print_categorie(self, cat_id):
         dialog = CategorieDialog(self.parent, self.db, cat_id)
