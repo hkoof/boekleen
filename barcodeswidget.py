ee354eadd4aef997a5927522dc07b53481e3daed
Heiko Noordhof <h.e.noordhof@rug.nl>
2019-08-29 12:37:54 +0200

    selection uitlezen uit barcode lijst werkt nu

--- barcodeswidget.py
+++ barcodeswidget.py
@@ -19,8 +19,12 @@ class BarcodesWidget(Gtk.Grid):
                 ColumnDef("Opmerking", "notes"),
             )
         self.lijst = DataLijst(columns, show_primary_key=True, expand=True, multiselect=True)
+        self.selection = self.lijst.view.get_selection()
+        self.selection.connect("changed", self.on_select_row)
         self.lijst.view.connect("row-activated", self.on_row_activated)
+        #self.lijst.view.connect("cursor-changed", self.on_select_row)
 
+    
         nieuw_button = Gtk.Button("Genereer nieuwe barcodes")
         nieuw_button.connect("clicked", self.on_nieuw)
         verwijder_button = Gtk.Button("Verwijder")
@@ -52,15 +56,32 @@ class BarcodesWidget(Gtk.Grid):
         selectie.attach(self.bestaand_check, 1, 0, 1, 1)
         selectie.attach(self.geprint_check, 1, 1, 1, 1)
 
-        paneel = Gtk.Grid(
-                margin=8,
+        markeren = Gtk.Grid(
+                margin=12,
                 column_spacing=8,
                 row_spacing=8,
-                halign=Gtk.Align.END,
             )
-        #paneel.attach(Gtk.Label(), 0, 0, 1, 1)
-        paneel.attach(selectie, 0, 0, 1, 1)
-        paneel.attach(knoppen, 1, 0, 1, 1)
+
+        markeren_paneel = Gtk.Frame(label="Markeren")
+        selectie_paneel = Gtk.Frame(label="Selecteren")
+        actie_paneel = Gtk.Frame(label="Acties")
+
+        markeren_paneel.add(markeren)
+        selectie_paneel.add(selectie)
+        actie_paneel.add(knoppen)
+
+        paneel = Gtk.Grid(
+                margin=16,
+                column_spacing=16,
+                row_spacing=16,
+                orientation=Gtk.Orientation.HORIZONTAL,
+                hexpand=True,
+                #halign=Gtk.Align.END,
+            )
+        paneel.attach(selectie_paneel, 0, 0, 1, 1)
+        paneel.attach(markeren_paneel, 1, 0, 1, 1)
+        paneel.attach(Gtk.Label(hexpand=True), 2, 0, 1, 1)
+        paneel.attach(actie_paneel, 3, 0, 1, 1)
 
         self.add(paneel)
         self.add(self.lijst)
@@ -74,6 +95,18 @@ class BarcodesWidget(Gtk.Grid):
                 printed=printed,
             )
         self.lijst.load(self.barcodes)
+
+    def get_selected(self):
+        model, path = self.selection.get_selected_rows()
+        if not path:
+            return []
+        result = list()
+        for row in path:
+            result.append(tuple([col for col in model[row]]))
+        return result
+
+    def scan_selected_rows(self):
+        print(self.get_selected())
 
     def on_verwijder(self, button):
         print("verwijderen barcode niet geimplemtneerd nog")
@@ -90,12 +123,10 @@ class BarcodesWidget(Gtk.Grid):
         self.refresh()
 
     def on_print(self, button):
-        model, path = self.lijst.view.get_selection().get_selected_rows()
-        if not path:
-            return
-        record = self.lijst.model[path]
-        cat_id = record[0]
-        self.print_categorie(cat_id)
+        pass
+
+    def on_select_row(self, view):
+        self.scan_selected_rows()
 
     def on_row_activated(self, view, path, column):
         #record = self.lijst.model[path]
@@ -103,9 +134,9 @@ class BarcodesWidget(Gtk.Grid):
         begin_idx = path.get_indices()[0]
         end_idx = min(begin_idx + self.labels_per_page - 1, max_idx)
 
-        selection = self.lijst.view.get_selection()
         endpath = Gtk.TreePath(end_idx)
-        selection.select_range(path, endpath)
+        self.selection.select_range(path, endpath)
+        self.scan_selected_rows()
 
     def print_categorie(self, cat_id):
         dialog = CategorieDialog(self.parent, self.db, cat_id)
