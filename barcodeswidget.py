c16c361121dcd2d13288712405c6b4617a681800
Heiko Noordhof <h.e.noordhof@rug.nl>
2019-08-29 16:09:54 +0200

    printen berekenen aantal label per pagina werkt nu

--- barcodeswidget.py
+++ barcodeswidget.py
@@ -3,6 +3,7 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import GLib, Gio, Gtk, Gdk
 
 from datalijst import DataLijst, ColumnDef
+from barcodeprint import BarcodePrinter
 
 class BarcodesWidget(Gtk.Grid):
     def __init__(self, parent, database):
@@ -10,7 +11,8 @@ class BarcodesWidget(Gtk.Grid):
         self.parent = parent
         self.db = database
         self.invalidated = False
-        self.labels_per_page = 6
+
+        self.printer = BarcodePrinter()
 
         columns = (
                 ColumnDef("Code", "isbn", datatype=str),
@@ -24,12 +26,12 @@ class BarcodesWidget(Gtk.Grid):
         #self.lijst.view.connect("cursor-changed", self.on_select_row)
 
     
-        nieuw_button = Gtk.Button("Genereer nieuwe barcodes")
-        nieuw_button.connect("clicked", self.on_nieuw)
+        self.nieuw_button = Gtk.Button("Genereer nieuwe barcodes")
+        self.nieuw_button.connect("clicked", self.on_nieuw)
         self.verwijder_button = Gtk.Button("Verwijder")
         self.verwijder_button.connect("clicked", self.on_verwijder)
-        print_button = Gtk.Button("Print")
-        print_button.connect("clicked", self.on_print)
+        self.print_button = Gtk.Button("Print")
+        self.print_button.connect("clicked", self.on_print)
 
         self.bestaand_check = Gtk.CheckButton(label="Ook bestaande boeken")
         self.bestaand_check.connect("clicked", self.refresh)
@@ -43,30 +45,30 @@ class BarcodesWidget(Gtk.Grid):
                 column_spacing=8,
                 row_spacing=8,
             )
-        knoppen.attach(nieuw_button, 1, 0, 1, 1)
+        knoppen.attach(self.nieuw_button, 1, 0, 1, 1)
         knoppen.attach(self.verwijder_button, 1, 1, 1, 1)
-        knoppen.attach(print_button, 1, 2, 1, 1)
+        knoppen.attach(self.print_button, 1, 2, 1, 1)
 
-        selectie = Gtk.Grid(
+        filter_grid = Gtk.Grid(
                 margin=12,
                 column_spacing=8,
                 row_spacing=8,
             )
-        selectie.attach(self.bestaand_check, 1, 0, 1, 1)
-        selectie.attach(self.geprint_check, 1, 1, 1, 1)
+        filter_grid.attach(self.bestaand_check, 1, 0, 1, 1)
+        filter_grid.attach(self.geprint_check, 1, 1, 1, 1)
 
-        markeren = Gtk.Grid(
+        selecteren_grid = Gtk.Grid(
                 margin=12,
                 column_spacing=8,
                 row_spacing=8,
             )
 
-        markeren_paneel = Gtk.Frame(label="Markeren")
-        selectie_paneel = Gtk.Frame(label="Selecteren")
+        selecteren_paneel = Gtk.Frame(label="Selecteren")
+        selectie_paneel = Gtk.Frame(label="Filteren")
         actie_paneel = Gtk.Frame(label="Acties")
 
-        markeren_paneel.add(markeren)
-        selectie_paneel.add(selectie)
+        selecteren_paneel.add(selecteren_grid)
+        selectie_paneel.add(filter_grid)
         actie_paneel.add(knoppen)
 
         paneel = Gtk.Grid(
@@ -75,10 +77,9 @@ class BarcodesWidget(Gtk.Grid):
                 row_spacing=16,
                 orientation=Gtk.Orientation.HORIZONTAL,
                 hexpand=True,
-                #halign=Gtk.Align.END,
             )
         paneel.attach(selectie_paneel, 0, 0, 1, 1)
-        paneel.attach(markeren_paneel, 1, 0, 1, 1)
+        paneel.attach(selecteren_paneel, 1, 0, 1, 1)
         paneel.attach(Gtk.Label(hexpand=True), 2, 0, 1, 1)
         paneel.attach(actie_paneel, 3, 0, 1, 1)
 
@@ -94,6 +95,7 @@ class BarcodesWidget(Gtk.Grid):
                 printed=printed,
             )
         self.lijst.load(self.barcodes)
+        self.disable_print_button_if_needed()
 
     def get_selected_rows(self):
         model, path = self.selection.get_selected_rows()
@@ -104,13 +106,16 @@ class BarcodesWidget(Gtk.Grid):
             result.append(tuple([col for col in model[row]]))
         return result
 
-    def disable_delete_button_if_custom_code_in_selection(self):
+    def disable_delete_button_if_needed(self):
         enabled = True
         for row in self.get_selected_rows():
             if not row[2]:
                 enabled = False
                 break
         self.verwijder_button.set_sensitive(enabled)
+
+    def disable_print_button_if_needed(self):
+        self.print_button.set_sensitive(self.get_selected_rows())
 
     def on_verwijder(self, button):
         print("verwijderen barcode niet geimplemtneerd nog")
@@ -119,21 +124,26 @@ class BarcodesWidget(Gtk.Grid):
         pass
 
     def on_print(self, button):
-        codes = self.get_selected_rows()
+        codes = [row[0] for row in self.get_selected_rows()]
+        self.printer.print(codes)
         for code in codes:
-            self.barcodes
-
+            self.db.mark_barcode_printed(code)
+        self.refresh()
 
     def on_select_row(self, view):
-        self.disable_delete_button_if_custom_code_in_selection()
+        self.disable_delete_button_if_needed()
+        self.disable_print_button_if_needed()
+
+    def get_labels_per_page(self):
+        self.printer.loadconfig()
+        return self.printer.get_codes_per_page()
 
     def on_row_activated(self, view, path, column):
-        #record = self.lijst.model[path]
         max_idx = max(len(self.barcodes) - 1, 0)
         begin_idx = path.get_indices()[0]
-        end_idx = min(begin_idx + self.labels_per_page - 1, max_idx)
+        end_idx = min(begin_idx + self.get_labels_per_page() - 1, max_idx)
 
         endpath = Gtk.TreePath(end_idx)
         self.selection.select_range(path, endpath)
-        self.disable_delete_button_if_custom_code_in_selection()
+        self.disable_delete_button_if_needed()
 
