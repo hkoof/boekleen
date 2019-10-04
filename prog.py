e418cbd69c74d89598e6fd94be74cd952b4f3cd3
Heiko Noordhof <h.e.noordhof@rug.nl>
2019-10-04 11:28:08 +0200

    versienummer ook in prog.py (gtk app info)

--- prog.py
+++ prog.py
@@ -1,4 +1,4 @@
 name = 'boekleen'
-version = '0.1'
+version = '3'
 description = "Boekleen - uitleenadministratie voor kleine bieb's"
 default_sqlite_database_file = "/var/lib/boekleen/boekleen.sq3"
